a5b70867f4980379ddefb2d7cab4588003bec093
vismitap <vismita.prabhu@gmail.com>
2022-01-24 01:12:48 +0530

    [Vismita] Add: Matrix program added

--- Fellowship_CSharp_Programs/FunctionalProg/TwoDimensionalArray.cs
+++ Fellowship_CSharp_Programs/FunctionalProg/TwoDimensionalArray.cs
@@ -8,9 +8,35 @@ namespace Fellowship_CSharp_Programs.FunctionalProg
 {
     public class TwoDimensionalArray
     {
+        int row, column, i , j;
         public void Matrix()
         {
-            Console.WriteLine("Hello from Matrix");
+            Console.WriteLine("Enter the value of Row and Column: ");
+            row = Convert.ToInt32(Console.ReadLine());
+            column = Convert.ToInt32(Console.ReadLine());
+
+            int [,] arr = new int[row, column];
+
+            Console.WriteLine("Enter the elements: ");
+            for(i = 0; i < row; i++)
+            {
+                for(j = 0; j < column; j++)
+                {
+                    arr[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The Matrix is : ");
+            for (i = 0; i < row; i++)
+            {
+                for (j = 0; j < column; j++)
+                {
+                    Console.Write(arr[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
